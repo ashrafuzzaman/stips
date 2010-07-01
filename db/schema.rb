cfa6c407fc065a53edfd8ce72f57d8e290d412b0
Mushfiqul Haque <mushfique@gmail.com>
2010-07-01 03:58:38 +0200

    Re-added portfolio plugin

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100701003152) do
+ActiveRecord::Schema.define(:version => 20100701014639) do
 
   create_table "faqs", :force => true do |t|
     t.string   "title"
