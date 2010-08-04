47bd0452eb464620dd59616d9e885030b9a7a288
Mushfiqul Haque <mushfique@gmail.com>
2010-08-04 14:42:42 +0200

    Revert "Modified courses and show news, courses in homepage"
    This reverts commit 913d66c7f7c3800be0be7d0f2fed6e6e720295de.

--- db/migrate/20100803204035_add_amount_in_courses.rb
+++
@@ -1,9 +0,0 @@
-class AddAmountInCourses < ActiveRecord::Migration
-  def self.up
-    add_column :courses, :fee, :float, :default => 0
-  end
-
-  def self.down
-    remove_column(:courses, :fee)
-  end
-end
