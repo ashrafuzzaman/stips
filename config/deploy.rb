ac898c65fb2fac84240f5af7cc16f912fa37a2cd
Mushfiqul Haque <mushfique@gmail.com>
2010-08-07 23:21:07 +0200

    Capistrano added

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+set :application, 'StIPS'
+set :user, 'p37r5007'
+set :domain, 'stips.se'
+set :mongrel_port, 'put_your_assigned_mongrel_port_number_here'
+set :server_hostname, 'stips.se'
+
+set :git_account, 'mushfique'
+
+set :scm_passphrase,  Proc.new { Capistrano::CLI.password_prompt('password') }
+
+role :web, server_hostname
+role :app, server_hostname
+role :db, server_hostname, :primary => true
+
+default_run_options[:pty] = true
+set :repository,  "git@github.com:ashrafuzzaman/stips.git"
+set :scm, "git"
+set :user, user
+
+ssh_options[:forward_agent] = true
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
+set :use_sudo, false
+set :deploy_to, "/home/#{user}/#{application}"
