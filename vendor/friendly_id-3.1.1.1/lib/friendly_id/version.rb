ae86684d3b68b41c922e2c4ee804de5f2a11d6e8
A.K.M. Ashrafuzzaman <jitu@dragon.(none)>
2010-08-10 21:21:56 +0200

    removed all the gems from the vendor

--- vendor/friendly_id-3.1.1.1/lib/friendly_id/version.rb
+++
@@ -1,9 +0,0 @@
-module FriendlyId
-  module Version
-    MAJOR = 3
-    MINOR = 1
-    TINY = 1
-    BUILD = 1
-    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
-  end
-end
--- vendor/plugins/refinerycms-poll/app/models/poll.rb
+++ vendor/plugins/refinerycms-poll/app/models/poll.rb
@@ -9,8 +9,8 @@ class Poll < ActiveRecord::Base
 
   def self.active_poll(user_identifier)
     users_polls = UsersPoll.find_all_by_user_identifier(user_identifier, :select => :poll_id)
-    polls = users_polls.count > 0 ? Poll.find(:all, :conditions => ["id NOT IN (?)", users_polls.collect {|up| up.poll_id } ]) : Poll.all
-    polls.count > 0 ? polls.first : nil
+    polls = users_polls.length > 0 ? Poll.find(:all, :conditions => ["id NOT IN (?)", users_polls.collect {|up| up.poll_id } ]) : Poll.all
+    polls.length > 0 ? polls.first : nil
   end
 
   def total_vote
