d91ef794e2588989103241dd787b2c08da87159e
A.K.M. Ashrafuzzaman <jitu@dragon.(none)>
2010-09-17 15:14:41 +0200

    Added mailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -8,3 +8,12 @@ require File.expand_path('../application', __FILE__)
 #  :user_name      => "no-reply",
 #  :password       => "stips01"
 #}
+
+ActionMailer::Base.smtp_settings = {
+  :address        => "mail.stips.se",
+  :port           => "26",
+  :authentication => :plain,
+  :user_name      => "no-reply",
+  :password       => "stips01",
+  :domain         => "stips.se"
+}
